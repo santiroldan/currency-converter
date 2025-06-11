e549e460f5de40a5dd949d9208848bca80911ee3
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-11 20:14:51 +0200

    feat: add CurrencyController for handling currency conversion requests

---
+++ src/presentation/currency-controller.ts
@@ -0,0 +1,33 @@
+import type { NextFunction, Request, Response } from "express";
+import type { CurrencyConverter } from "../application/currency-converter";
+
+export class CurrencyController {
+	constructor(private convertCurrency: CurrencyConverter) {}
+
+	public async convert(req: Request, res: Response, next: NextFunction) {
+		const { from, to, amount } = req.query;
+
+		if (!from || !to || !amount) {
+			return res
+				.status(400)
+				.json({ error: "from, to and amount query params are required" });
+		}
+
+		try {
+			const result = await this.convertCurrency.run({
+				from: String(from),
+				to: String(to),
+				amount: Number(amount),
+			});
+			res.json({ result });
+		} catch (error) {
+			let message = String(error);
+
+			if (error instanceof Error) {
+				message = error.message;
+			}
+
+			res.status(500).json({ error: message });
+		}
+	}
+}
