5b9682c76d38794da1a35de044d1c56bae1beb16
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-11 21:52:31 +0200

    refactor: update import path for app in exchange rate e2e test

--- test/infrastructure/http/exchange-rate.e2e-spec.ts
+++ test/infrastructure/http/exchange-rate.e2e-spec.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
 import { describe, expect, it } from "vitest";
-import { app } from "../../src/app";
+import { app } from "../../../src/app";
 
 describe("GET /convert", () => {
 	it("should return a converted amount", async () => {
